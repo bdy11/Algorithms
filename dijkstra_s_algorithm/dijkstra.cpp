53d565cfe4be65159c467c88db79415a67ab49b5
dongyue.bai <dongyue.bai@zongmutech.com>
2025-04-27 11:17:00 +0200

    update dijkstra' algorithm

--- dijkstra_s_algorithm/dijkstra.cpp
+++ dijkstra_s_algorithm/dijkstra.cpp
@@ -1,242 +1,157 @@
-/*-------------------------------------------------------
-                    <copyright notice>
-
-Created by Dongyue Bai
-date: 18.01.2019
-In this work, adjacency matrix representation is used for graph representation. 
-
----------------------------------------------------------*/
-#include <cstdlib>
 #include <iostream>
-#include <ctime>
+#include <vector>
 #include <random>
+#include <ctime>
 #include <limits>
 
-
-//creat a class for graph
-class Graph{
+class Graph {
 public:
+    Graph(int size, double density, double distance_low, double distance_high)
+        : m_size(size),
+          m_density(density),
+          m_distance_low(distance_low),
+          m_distance_high(distance_high),
+          m_graph(size, std::vector<double>(size, 0.0)),
+          dist(size, std::numeric_limits<double>::max()),
+          visited(size, false)
+    {}
 
-bool *ShortestPathTree; //visited vertices will be marked as true, unvisited vertices will be marked as false
-double *dist;  // used to store the distance from source to i-th element
-
-//constructors
-Graph();
-
-Graph(int _size,double _density, double _distance_range_low, double _distance_range_high): 
-m_size(_size), m_density(_density), m_distance_range_low(_distance_range_low),m_distance_range_high(_distance_range_high){};  
-
-
-// function that draws a randomly generated 2D graph *m_graph[] and put edges in the graph by a density
-void f_draw_graph(); 
-
-//function that prints the generated graph
-void f_print_graph(); 
-
-//function that checks if the graph is connected
-bool f_is_connected(); 
-
-// function that implments dijkstras algorithm
-void dijkstra();
-
-
-//destructor
-~Graph(){
-    delete[] m_graph;
-};
-
+    void drawGraph();
+    void printGraph() const;
+    bool isConnected() const;
+    void dijkstra() const;
 
 private:
-double **m_graph;
-int m_size;
-double m_density;
-double m_distance_range_low;
-double m_distance_range_high;
+    int m_size;
+    double m_density;
+    double m_distance_low;
+    double m_distance_high;
+    std::vector<std::vector<double>> m_graph;
+    mutable std::vector<double> dist;
+    mutable std::vector<bool> visited;
 };
 
+void Graph::drawGraph() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(m_distance_low, m_distance_high);
+    std::uniform_real_distribution<> prob(0.0, 1.0);
 
-void Graph::f_draw_graph()
-{
-//create a graph
-m_graph= new double*[m_size];
-for(int i=0;i<m_size;++i)  {m_graph[i]=new double[m_size];};    
-
- //seed rand()
-srand(time(0));
-
-//put edges in the graph
-for(int i=0;i<m_size;++i)
-{
-   for(int j=0;j<m_size;++j)
-   {
-        if(i==j) 
-        {
-            m_graph[i][j]=0;  
+    for (int i = 0; i < m_size; ++i) {
+        for (int j = i + 1; j < m_size; ++j) {
+            if (prob(gen) < m_density) {
+                double distance = dis(gen);
+                m_graph[i][j] = distance;
+                m_graph[j][i] = distance; // symmetric
+            }
         }
-        else 
-        {
-            
-            bool edge=((rand() % 100) < m_density*100);  // the probability of edge to be 1 is the value of density
-            
-            double f = (double)rand()/RAND_MAX;
-            double distance= m_distance_range_low + f * (m_distance_range_high - m_distance_range_low);
-            m_graph[i][j]=m_graph[j][i]=(static_cast<double> (edge))*distance;
-        }
-    }     
-}
+    }
 }
 
-void Graph::f_print_graph()
-{
-    for(int i=0;i<m_size;++i){
-        for(int j=0;j<m_size;++j){
-            std::cout<<m_graph[i][j]<<"\t";
+void Graph::printGraph() const {
+    for (const auto& row : m_graph) {
+        for (double val : row) {
+            std::cout << val << "\t";
         }
-        std::cout<<std::endl;
+        std::cout << "\n";
     }
-} 
+}
 
+bool Graph::isConnected() const {
+    std::vector<bool> open(m_size, false);
+    std::vector<bool> closed(m_size, false);
+    int closed_size = 0;
 
-//check if the graph is connected
-bool Graph::f_is_connected()  
-{
-    int old_size, c_size=0;
-    bool* close=new bool[m_size];
-    bool* open=new bool[m_size];
-    for(int i=0;i<m_size;++i)
-    {
-        close[i]=open[i]=false; //initialization, both close set and open set are empty
-    }
-    open[0]=true;
+    open[0] = true;
 
-
-    while(c_size<m_size)
-    {
-        for(int i=0;i<m_size;++i)
-        {
-            old_size=c_size;
-            if(open[i]&&(close[i]==false))
-            {   close[i]=true; 
-                c_size++;    //add to close set 
-                for(int j=0;j<m_size;++j)
-                {
-                    open[j]=open[j]||(m_graph[i][j]>0);   //add to open set
+    while (closed_size < m_size) {
+        int prev_closed_size = closed_size;
+        for (int i = 0; i < m_size; ++i) {
+            if (open[i] && !closed[i]) {
+                closed[i] = true;
+                ++closed_size;
+                for (int j = 0; j < m_size; ++j) {
+                    if (m_graph[i][j] > 0) {
+                        open[j] = true;
+                    }
                 }
             }
         }
-        if(c_size==m_size)    {return true;}  //if all nodes in close set, connected
-        if(c_size== old_size) {return false;}
-       
+        if (closed_size == prev_closed_size) {
+            return false; // no progress
+        }
     }
+    return true;
 }
 
+void Graph::dijkstra() const {
+    dist.assign(m_size, std::numeric_limits<double>::max());
+    visited.assign(m_size, false);
 
+    dist[0] = 0.0;
 
+    for (int count = 0; count < m_size - 1; ++count) {
+        double min_dist = std::numeric_limits<double>::max();
+        int min_index = -1;
 
-// Function that implements Dijkstra's single source shortest path algorithm 
-// for a graph represented using adjacency matrix representation 
-void Graph::dijkstra()
-{
-    bool* ShortestPathTree=new bool[m_size];
-    double* dist=new double[m_size];
-
-     // Initialize all distances as INTMAX and ShortestPathTree[] as false 
-     for (int i = 0; i <m_size; i++)
-     {
-        dist[i] = std::numeric_limits<int>::max();
-        ShortestPathTree[i] = false; 
-     }
-     
-     // Distance of source vertex from itself is always 0 
-     dist[0] = 0; 
-
-     // Find shortest path for all vertices 
-     for (int count = 0; count < m_size-1; ++count) 
-     { 
-
-       // Pick the minimum distance vertex from the set of vertices not yet visited. 
-        double min = std::numeric_limits<int>::max();
-        int min_index; 
-        for (int i = 0; i < m_size; ++i) 
-        {
-            if (!ShortestPathTree[i] && dist[i]<=min )
-                {
-                    min = dist[i]; 
-                    min_index = i;                     
-                }             
-        }
-
-       int u = min_index; 
-   
-       // Mark the picked vertex as visited 
-       ShortestPathTree[u] = true; 
-   
-       // Update dist value of the adjacent vertices of the picked vertex. 
-       for (int i = 0; i < m_size; ++i)
-       {   
-         // Update dist[i] only if is not in ShortestPathTree, there is an edge from  
-         // u to i, and total weight of path from source to  i through u is  
-         // smaller than current value of dist[i] 
-         if (!ShortestPathTree[i] && m_graph[u][i] && dist[u]+m_graph[u][i] < dist[i])
-            { 
-            dist[i] = dist[u] + m_graph[u][i]; 
+        for (int i = 0; i < m_size; ++i) {
+            if (!visited[i] && dist[i] < min_dist) {
+                min_dist = dist[i];
+                min_index = i;
             }
         }
-     } 
 
-    //print out the result
-    double sum, aver_dist;
-    std::cout<<"Vertex Distance from Source"<<std::endl; 
-    for (int i = 0; i < m_size; ++i)
-    { 
-        std::cout<<i<<" "<<dist[i]<<std::endl; 
+        if (min_index == -1) {
+            break; // Remaining nodes are unreachable
+        }
+
+        visited[min_index] = true;
+
+        for (int i = 0; i < m_size; ++i) {
+            if (!visited[i] && m_graph[min_index][i] > 0 &&
+                dist[min_index] + m_graph[min_index][i] < dist[i]) {
+                dist[i] = dist[min_index] + m_graph[min_index][i];
+            }
+        }
+    }
+
+    double sum = 0.0;
+    std::cout << "Vertex\tDistance from Source\n";
+    for (int i = 0; i < m_size; ++i) {
+        std::cout << i << "\t" << dist[i] << "\n";
         sum += dist[i];
     }
-    aver_dist=sum/49;
 
-    std::cout<<"the average distance is "<<aver_dist<<std::endl;
-    
-   
-} 
+    double average_dist = sum / m_size;
+    std::cout << "Average distance: " << average_dist << "\n";
+}
 
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
 
+    std::cout << "Draw a graph: 20% density, 50 nodes, distance range 1.0 to 10.0\n";
+    Graph g1(50, 0.2, 1.0, 10.0);
+    g1.drawGraph();
+    g1.printGraph();
 
-int main(){
-    std::cout<< "draw a graph:20% of 50 nodes with a distance range of 1.0 to 10.0"<<std::endl; 
-    //generate graph
-    Graph obj_graph(50,0.2,1,10);
-    obj_graph.f_draw_graph();
-    obj_graph.f_print_graph();
-
-    //check if the graph is connected
-    bool connection_status=obj_graph.f_is_connected();
-    if (connection_status==true){
-        std::cout<<"The generated graph is connected"<<std::endl;
-        }
-    else{std::cout<<"The generated graph is not connected"<<std::endl;}
-
-    if(connection_status==true)
-    {
-        obj_graph.dijkstra();
-    }
-    
-    
-    std::cout<< "draw a graph: 40% of 50 nodes with a distance range of 1.0 to 10.0"<<std::endl; 
-    //generate graph
-    Graph obj2_graph(50,0.4,1,10);
-    obj2_graph.f_draw_graph();
-    obj2_graph.f_print_graph();
-
-    //check if the graph is connected
-    connection_status=obj2_graph.f_is_connected();
-    if (connection_status==true){
-        std::cout<<"The generated graph is connected"<<std::endl;
-        }
-    else{std::cout<<"The generated graph is not connected"<<std::endl;}
-
-    if(connection_status==true)
-    {
-        obj2_graph.dijkstra();
+    if (g1.isConnected()) {
+        std::cout << "The generated graph is connected.\n";
+        g1.dijkstra();
+    } else {
+        std::cout << "The generated graph is not connected.\n";
     }
 
+    std::cout << "\nDraw a graph: 40% density, 50 nodes, distance range 1.0 to 10.0\n";
+    Graph g2(50, 0.4, 1.0, 10.0);
+    g2.drawGraph();
+    g2.printGraph();
+
+    if (g2.isConnected()) {
+        std::cout << "The generated graph is connected.\n";
+        g2.dijkstra();
+    } else {
+        std::cout << "The generated graph is not connected.\n";
+    }
+
+    return 0;
 }
